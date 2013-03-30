55b64158158d7ca20b7e1717324935d4ee9a4a3b
Anthony <frehnar@hotmail.com>
2013-03-29 19:07:27 -0600

    Got it to work finally

--- src/edu/byu/isys413/afreh20/mobapp/MainActivity.java
+++ src/edu/byu/isys413/afreh20/mobapp/MainActivity.java
@@ -1,15 +1,42 @@
 package edu.byu.isys413.afreh20.mobapp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ViewFlipper;
+
 
 public class MainActivity extends Activity {
-
+	
+	ViewFlipper vf = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		vf = (ViewFlipper)findViewById(R.id.vf);
 	}
 
 	@Override
@@ -17,6 +44,11 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void loginbtnclick(View view){
+		System.out.println("login");
+		vf.setDisplayedChild(1);
 	}
 
 }
