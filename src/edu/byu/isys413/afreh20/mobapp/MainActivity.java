f20a1c6599c64e265d59e6a90743ab7dcc1ad2e5
Anthony <frehnar@hotmail.com>
2013-04-04 20:21:52 -0600

    I have the pic taking and viewing working.

--- src/edu/byu/isys413/afreh20/mobapp/MainActivity.java
+++ src/edu/byu/isys413/afreh20/mobapp/MainActivity.java
@@ -1,7 +1,9 @@
 package edu.byu.isys413.afreh20.mobapp;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -25,17 +27,24 @@ import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
 
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.MediaStore.Images.Media;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.text.Editable;
+import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
@@ -51,6 +60,8 @@ public class MainActivity extends Activity {
 	boolean loggedIn = false;
 	ArrayList<HashMap<String, String>> custPics = new ArrayList<HashMap<String, String>>();
 	ArrayList<String> picNames = new ArrayList<String>();
+	private static final int TAKE_PHOTO_CODE = 1;
+	String tempPic64;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -87,17 +98,56 @@ public class MainActivity extends Activity {
 		}
 //		
 	}
-	
-	public void btntakepicclick(View view){
-		
-//		vf.setDisplayedChild(0);
+
+	private File getTempFile(Context context) {
+		// it will return /sdcard/image.tmp
+		final File path = new File(Environment.getExternalStorageDirectory(), context.getPackageName());
+		if (!path.exists()) {
+			path.mkdir();
+		}
+		return new File(path, "image.tmp");
 	}
 	
+	public void btntakepicclick(View view){
+		final Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(getTempFile(this)));
+		startActivityForResult(intent, TAKE_PHOTO_CODE);
+//			vf.setDisplayedChild(0);
+	}
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == RESULT_OK && requestCode == TAKE_PHOTO_CODE) {
+			File file = getTempFile(this);
+			try {
+//				System.out.println("camera stuff");
+//				showToast("camera success?");
+				Bitmap captureBmp = Media.getBitmap(getContentResolver(), Uri.fromFile(file));
+				// ImageView viewer = (ImageView) findViewById(R.id.pictureViewer);
+				ByteArrayOutputStream out = new ByteArrayOutputStream();
+				// viewer.setImageBitmap(captureBmp);
+				InputStream in = new FileInputStream(file);
+				int count;
+				byte[] buffer = new byte[512];
+				while ((count = in.read(buffer)) >= 0) {
+					out.write(buffer, 0, count);
+				}
+				tempPic64 = Base64.encodeToString(out.toByteArray(), Base64.DEFAULT);
+				vf.setDisplayedChild(3);
+				ImageView imgView = (ImageView) findViewById(R.id.imageView);
+				imgView.setImageBitmap(captureBmp);
+				
+			} catch (Exception e){
+				e.printStackTrace();
+				showToast("fail in camera");
+			}
+		}
+	}
+
 	public void viewalbum(View view){
 		vf.setDisplayedChild(2);
-		if(picNames.size() == 0){
-			picNames.add("No pics uploaded yet");
-		}
+//		if(picNames.size() == 0){
+//			picNames.add("No pics uploaded yet");
+//		}
 		ListView listView = (ListView) findViewById(R.id.album);
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, picNames);
 		listView.setAdapter(adapter);
